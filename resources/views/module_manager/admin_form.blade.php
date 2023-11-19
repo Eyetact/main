e7d1bc5f89165c7f1609db392ef5c972c0333359
Pragna <brahm.pragya@gmail.com>
2023-11-19 23:04:03 +0530

    admin-form

--- resources/views/module_manager/admin_form.blade.php
+++ resources/views/module_manager/admin_form.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-12 col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Admin</h3>
+                    <h3 class="card-title">Admin </h3>
                     &nbsp &nbsp
                     <span id="currentEditName"></span>
                 </div>
