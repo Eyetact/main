ac86f9ac63f9e855826b4b1bc9827b88e82a5a29
ayaeyt6 <ayaeyt6@gmail.com>
2024-01-07 04:18:17 -0800

    fix

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -9,21 +9,5 @@ class Attribute extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'module',
-        'name',
-        'field_type',
-        'input_name',
-        'input_class',
-        'input_id',
-        'is_required',
-        'is_system',
-        'fields_info',
-        'is_enable',
-        'scope',
-        'depend',
-        'attribute',
-        'validation',
-        'description'
-    ];
+    protected $guarded = [];
 }
