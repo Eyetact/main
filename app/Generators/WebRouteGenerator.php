5f0cd0c01e1f5e18b9b433f4982dffc8e5e9ab27
bawanehcod <a.al-bawaneh@bottletech.net>
2024-01-07 12:23:27 +0300

    fix

--- app/Generators/WebRouteGenerator.php
+++ app/Generators/WebRouteGenerator.php
@@ -21,11 +21,9 @@ class WebRouteGenerator
         $modelNameSingularPascalCase = GeneratorUtils::singularPascalCase($model);
         $modelNamePluralLowercase = GeneratorUtils::pluralKebabCase($model);
 
-        if ($path != '') {
-            $controllerClass = "\n" . "Route::resource('" . $path . "', App\Http\Controllers\Admin\\" . str_replace('/', '\\', $path) . "\\" . $modelNameSingularPascalCase . "Controller::class)->middleware('auth');";
-        } else {
-            $controllerClass = "\n" . "Route::resource('" . $path . "', App\Http\Controllers\Admin\\" . $modelNameSingularPascalCase . "Controller::class)->middleware('auth');";
-        }
+
+        $controllerClass = "\n" . "Route::resource('" . $path . "', App\Http\Controllers\Admin\\" . $modelNameSingularPascalCase . "Controller::class)->middleware('auth');";
+
 
         File::append(base_path('routes/web.php'), $controllerClass);
     }
@@ -52,6 +50,6 @@ class WebRouteGenerator
             $controllerClass = "\n" . "Route::resource('" . $modelNamePluralLowercase . "', App\Http\Controllers\Admin\\" . $modelNameSingularPascalCase . "Controller::class)->middleware('auth');";
         }
 
-        File::replaceInFile($controllerClass,'',base_path('routes/web.php'));
+        File::replaceInFile($controllerClass, '', base_path('routes/web.php'));
     }
 }
