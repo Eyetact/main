82fc4038a8b23039a5fc527464661be57a2fac6d
bawanehcod <a.al-bawaneh@bottletech.net>
2024-02-06 12:51:32 +0300

    fix

--- routes/generator.php
+++ routes/generator.php
@@ -1,2 +1,6 @@
 <?php
 
+
+Route::get('test', [ App\Http\Controllers\Admin\TestController::class, 'index' ])->middleware('auth');
+
+Route::resource('tests', App\Http\Controllers\Admin\TestController::class)->middleware('auth');
