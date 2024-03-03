6899c7c53e7083234771676a4a466fa0612ce201
bawanehcod <a.al-bawaneh@bottletech.net>
2024-03-03 16:06:09 +0300

    admin

--- app/Generators/Views/FormViewGenerator.php
+++ app/Generators/Views/FormViewGenerator.php
@@ -1130,10 +1130,10 @@ class FormViewGenerator
 
                                             $totalOptions = count($arrOption);
                                             $template .= '<td><div class="input-box">';
-                                            $template .= ' <select name="' . $field->code . '[{{ $index }}][' . $value->name . ']" class="form-select  google-input multi-type" required="">';
+                                            $template .= ' <select name="' . $field->code . '[{{ $index }}][' . $value->code . ']" class="form-select  google-input multi-type" required="">';
 
                                             $template.= '@foreach( \\App\\Models\\Admin\\'.GeneratorUtils::singularPascalCase($value->constrain).'::all() as $item2 )';
-                                                $template .= '<option @selected( isset($item->'.$value->name.') ? $item->'.$value->name.' == "$item2->'.$value->attribute . '" : 0 )  value="{{ $item2->'.$value->attribute . '}}" >{{ $item2->'.$value->attribute . '}}</option>';
+                                                $template .= '<option @selected( isset($item->'.$value->code.') ? $item->'.$value->code.' == "$item2->'.$value->attribute . '" : 0 )  value="{{ $item2->'.$value->attribute . '}}" >{{ $item2->'.$value->attribute . '}}</option>';
 
                                             $template.= '@endforeach';
                                             $template .= '</select>';
