0ad3bbf77fd242c86b349918a50ac5d016bfa451
ayamatroud2024 <githubaya@gmail.com>
2024-04-28 16:08:15 +0300

    fix

--- resources/views/layouts/aside-menu.blade.php
+++ resources/views/layouts/aside-menu.blade.php
@@ -57,6 +57,10 @@
         </li>
 
 
+
+
+
+
         {{-- <li class="slide">
             <a class="side-menu__item" href="{{ url('data') }}">
                 <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" height  ="24" viewBox="0 0 24 24"
